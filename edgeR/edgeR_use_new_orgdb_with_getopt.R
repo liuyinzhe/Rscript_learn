6e396b0aea4fb61501ea4598e11df162f78d6be0
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-06-18 14:19:41 +0800

    Update edgeR_use_new_orgdb_with_getopt.R

--- edgeR/edgeR_use_new_orgdb_with_getopt.R
+++ edgeR/edgeR_use_new_orgdb_with_getopt.R
@@ -536,6 +536,148 @@ plotGOgraph(goMF)
 dev.off()
 ############################总的 GO ############################
 
+## shorten the names of GO terms
+
+shorten_names <- function(x, n_word=4, n_char=40){
+  if (length(strsplit(x, " ")[[1]]) > n_word || (nchar(x) > 40))
+  {
+    if (nchar(x) > 40) x <- substr(x, 1, 40)
+    x <- paste(paste(strsplit(x, " ")[[1]][1:min(length(strsplit(x," ")[[1]]), n_word)],
+                       collapse=" "), "...", sep="")
+    return(x)
+  } 
+  else
+  {
+    return(x)
+  }
+}
+
+GO_Up <- enrichGO(gene=Up_ids,
+               OrgDb=org.Saureus.eg.db,
+               keyType="GID",
+               ont="ALL",   #CC/BP/MF可选
+               pvalueCutoff = 1, # 默认值0.05
+               qvalueCutoff = 1,  # 默认值0.2，是pvalue 的校正值 更严格
+               minGSSize = 10)  # 默认值，富集的最小基因数量
+
+go_MF<-GO_Up[GO_Up$ONTOLOGY=="MF",][1:10,]
+go_CC<-GO_Up[GO_Up$ONTOLOGY=="CC",][1:10,]
+go_BP<-GO_Up[GO_Up$ONTOLOGY=="BP",][1:10,]
+
+go_enrich_df<-data.frame(ID=c(go_BP$ID, go_CC$ID, go_MF$ID),
+                         Description=c(go_BP$Description, go_CC$Description, go_MF$Description),
+                         GeneNumber=c(go_BP$Count, go_CC$Count, go_MF$Count),
+                         type=factor(
+                            c(rep("biological process", 10),
+                            rep("cellular component", 10),
+                            rep("molecular function",10)),
+                            levels=c("molecular function", "cellular component", "biological process")
+                            )
+                        )
+
+
+## numbers as data on x axis
+
+go_enrich_df$number <- factor(rev(1:nrow(go_enrich_df)))
+
+
+## shorten the names of GO terms
+# shorten_names <- function(x, n_word=4) {
+#     if (length(strsplit(x, " ")[[1]]) > n_word){
+#         return(paste(paste(strsplit(x, " ")[[1]][1:n_word], collapse=" "), "...", sep=""))
+#     }else{
+#         return(x)
+#     }
+# }
+
+labels=(sapply(
+  levels(as.factor(go_enrich_df$Description))[as.numeric(as.factor(go_enrich_df$Description))],
+  shorten_names))
+  
+names(labels) = rev(1:nrow(go_enrich_df))
+
+## colors for bar // green, blue, orange
+
+CPCOLS <- c("#8DA1CB", "#FD8D62", "#66C3A5")
+
+GO_Up_bar <- ggplot(data=go_enrich_df, aes(x=number, y=GeneNumber, fill=type)) +
+  geom_bar(stat="identity", width=0.8) + coord_flip() + 
+  scale_fill_manual(values = CPCOLS) + theme_test() + 
+  scale_x_discrete(labels=labels) +
+  xlab("GO term") + 
+  theme(axis.text=element_text(face = "bold", color="gray50")) +
+  labs(title = "The Most Enriched GO Terms")+
+  theme(
+    legend.position = "top",
+    legend.title=element_blank()
+  )
+
+ggsave(paste(GO_dir, paste(group_str,'GO_class_Up_bar.png', sep = "."), sep = "/"),plot=GO_Up_bar,height=10, width=15, units="in",dpi=300)
+ggsave(paste(GO_dir, paste(group_str,'GO_class_Up_bar.pdf', sep = "."), sep = "/"),plot=GO_Up_bar,height=10, width=15, units="in",dpi=300)
+
+GO_Down <- enrichGO(gene=Down_ids,
+               OrgDb=org.Saureus.eg.db,
+               keyType="GID",
+               ont="ALL",   #CC/BP/MF可选
+               pvalueCutoff = 1, # 默认值0.05
+               qvalueCutoff = 1,  # 默认值0.2，是pvalue 的校正值 更严格
+               minGSSize = 10)  # 默认值，富集的最小基因数量
+
+go_MF<-GO_Down[GO_Down$ONTOLOGY=="MF",][1:10,]
+go_CC<-GO_Down[GO_Down$ONTOLOGY=="CC",][1:10,]
+go_BP<-GO_Down[GO_Down$ONTOLOGY=="BP",][1:10,]
+
+go_enrich_df<-data.frame(ID=c(go_BP$ID, go_CC$ID, go_MF$ID),
+                         Description=c(go_BP$Description, go_CC$Description, go_MF$Description),
+                         GeneNumber=c(go_BP$Count, go_CC$Count, go_MF$Count),
+                         type=factor(
+                            c(rep("biological process", 10),
+                            rep("cellular component", 10),
+                            rep("molecular function",10)),
+                            levels=c("molecular function", "cellular component", "biological process")
+                            )
+                        )
+
+
+## numbers as data on x axis
+
+go_enrich_df$number <- factor(rev(1:nrow(go_enrich_df)))
+
+
+## shorten the names of GO terms
+# shorten_names <- function(x, n_word=4) {
+#     if (length(strsplit(x, " ")[[1]]) > n_word){
+#         return(paste(paste(strsplit(x, " ")[[1]][1:n_word], collapse=" "), "...", sep=""))
+#     }else{
+#         return(x)
+#     }
+# }
+
+labels=(sapply(
+  levels(as.factor(go_enrich_df$Description))[as.numeric(as.factor(go_enrich_df$Description))],
+  shorten_names))
+  
+names(labels) = rev(1:nrow(go_enrich_df))
+
+## colors for bar // green, blue, orange
+
+CPCOLS <- c("#8DA1CB", "#FD8D62", "#66C3A5")
+
+GO_Down_bar <- ggplot(data=go_enrich_df, aes(x=number, y=GeneNumber, fill=type)) +
+  geom_bar(stat="identity", width=0.8) + coord_flip() + 
+  scale_fill_manual(values = CPCOLS) + theme_test() + 
+  scale_x_discrete(labels=labels) +
+  xlab("GO term") + 
+  theme(axis.text=element_text(face = "bold", color="gray50")) +
+  labs(title = "The Most Enriched GO Terms")+
+  theme(
+    legend.position = "top",
+    legend.title=element_blank()
+  )
+
+ggsave(paste(GO_dir, paste(group_str,'GO_class_Down_bar.png', sep = "."), sep = "/"),plot=GO_Down_bar,height=10, width=15, units="in",dpi=300)
+ggsave(paste(GO_dir, paste(group_str,'GO_class_Down_bar.pdf', sep = "."), sep = "/"),plot=GO_Down_bar,height=10, width=15, units="in",dpi=300)
+
 
 ################################# Kegg注释 #####################################
 # kegg_enrich 富集分析
