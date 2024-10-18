c3cdcdcc6a03a480dda5bda5cd6b90eae156e1c7
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2024-10-18 08:31:39 +0800

    Update ColorCard.R

--- ColorCard.R
+++ ColorCard.R
@@ -17,6 +17,15 @@ qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 color74 = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 pie(rep(1,60), col=sample(color74, 60, replace = F))
 
+#50种
+color50 <- c(   "#ed1299", "#09f9f5", "#246b93", "#cc8e12", "#d561dd", "#c93f00", "#ddd53e", "#4aef7b",
+                "#e86502", "#9ed84e", "#39ba30", "#6ad157", "#8249aa", "#99db27", "#e07233", "#ff523f",
+                "#ce2523", "#f7aa5d", "#cebb10", "#03827f", "#931635", "#373bbf", "#a1ce4c", "#ef3bb6",
+                "#d66551", "#1a918f", "#ff66fc", "#2927c4", "#7149af" ,"#57e559" ,"#8e3af4" ,"#f9a270",
+                "#22547f", "#db5e92", "#edd05e", "#6f25e8", "#0dbc21", "#280f7a", "#6373ed", "#5b910f",
+                "#7b34c1" ,"#0cf29a" ,"#d80fc1", "#dd27ce", "#07a301", "#167275", "#391c82", "#2baeb5",
+                "#925bea", "#63ff4f")
+
 #37种
 color37 = c("#466791","#60bf37","#953ada","#4fbe6c","#ce49d3","#a7b43d","#5a51dc","#d49f36","#552095","#507f2d","#db37aa","#84b67c","#a06fda","#df462a","#5b83db","#c76c2d","#4f49a3","#82702d","#dd6bbb","#334c22","#d83979","#55baad","#dc4555","#62aad3","#8c3025","#417d61","#862977","#bba672","#403367","#da8a6d","#a79cd4","#71482c","#c689d0","#6b2940","#d593a7","#895c8b","#bd5975")
 pie(rep(1,37), col=sample(color37, 37))
